973d9ffcf3bea0a2ac2342b1f1e42b60e873347f
Shenny <shenny@MacBook-Pro-Shenny.local>
2025-10-18 03:28:48 +0800

    Add placeholder comment to main.py

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ from PyQt6.QtWidgets import QApplication
 
 from app.ui.main_window import MainWindow
 
-
+#check that
 def main() -> None:
     app = QApplication([])
     window = MainWindow(Path("app/settings.json"))
